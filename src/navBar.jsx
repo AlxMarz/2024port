17d37acb44a1810d7d4d1746c130933af6a1d984
alex marquez <marquez798@gmail.com>
2024-02-18 20:08:24 -0600

    styled

---
+++ src/navBar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar(){
+    return <nav className="nav">
+        <a href="/"> Alex M</a>
+        <ul>
+            <li>
+                <a href="/abc">abc</a>
+            </li>
+            <li>
+            <a href="/123">123</a>
+            </li>
+        </ul>
+
+    </nav>
+}
