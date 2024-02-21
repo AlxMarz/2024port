94f5cbb1ecf61d90eb25d893a065b475ea77c580
Alex Marquez <marquez798@gmail.com>
2024-02-21 15:01:55 -0600

    setting up portfoli

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,13 +3,7 @@ export default function AboutPage() {
   return (
     <div className="container pt-4">
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-        impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-        architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-        mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-        nostrum temporibus ad omnis nam rerum eligendi.
+        Hello these would be my feautred projects id want to show off
       </p>
       <section className="features-icons bg-light text-center m-4">
         <div className="container">
@@ -19,7 +13,7 @@ export default function AboutPage() {
                 <div className="features-icons-icon d-flex">
                   <i className="bi-window m-auto text-primary" />
                 </div>
-                <h3>Fully Responsive</h3>
+                <h3>Project 1</h3>
                 <p className="lead mb-0">
                   This theme will look great on any device, no matter the size!
                 </p>
@@ -30,7 +24,7 @@ export default function AboutPage() {
                 <div className="features-icons-icon d-flex">
                   <i className="bi-layers m-auto text-primary" />
                 </div>
-                <h3>Bootstrap 5 Ready</h3>
+                <h3>Project 2</h3>
                 <p className="lead mb-0">
                   Featuring the latest build of the new Bootstrap 5 framework!
                 </p>
@@ -41,7 +35,7 @@ export default function AboutPage() {
                 <div className="features-icons-icon d-flex">
                   <i className="bi-terminal m-auto text-primary" />
                 </div>
-                <h3>Easy to Use</h3>
+                <h3>Project 3</h3>
                 <p className="lead mb-0">
                   Ready to use with your own content, or customize the source
                   files!
@@ -52,13 +46,7 @@ export default function AboutPage() {
         </div>
       </section>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-        impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-        architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-        mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-        nostrum temporibus ad omnis nam rerum eligendi.
+        bellow this would be other repos id want to show off
       </p>
     </div>
   );
