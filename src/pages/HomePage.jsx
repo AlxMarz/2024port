eb3da786dd0c594a94554728616131de97cbd8f8
Alex Marquez <marquez798@gmail.com>
2024-02-20 19:28:00 -0600

    setting up styles for hero

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,42 +1,51 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 // Bringing in the required component from 'react-router-dom' for linking between pages
-import { Link } from 'react-router-dom';
-import Profile from '../components/UI/ProfileSections/ProfileTeaser';
-import ListItem from '../components/UI/ListItem';
+// import { Link } from 'react-router-dom';
+// import Profile from '../components/UI/ProfileSections/ProfileTeaser';
+// import ListItem from '../components/UI/ListItem';
 
-import API from '../utils/API';
+// import API from '../utils/API';
 
 export default function HomePage() {
   // Prior to the return statement, our homepage uses a few react hooks and fetchData function to query to a mock database and retrieve random user data
-  const [users, setUsers] = useState([]);
+  // const [users, setUsers] = useState([]);
 
-  const fetchData = async () => {
-    const { data } = await API.getUsers();
+  // const fetchData = async () => {
+  //   const { data } = await API.getUsers();
 
-    setUsers(data);
-  };
+  //   setUsers(data);
+  // };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   // Iterate over each mock user to display their abridged profile data and a link to their page
   return (
-    <div className="container pt-4">
-      <ul className="list-group list-group">
-        {users.map((user) => (
-          <ListItem key={user.id}>
-            <Profile user={user} />
-            {/* Link elements are anchors under-the-hood, but they allow the routing behavior to be controlled by the client rather than the server */}
-            <Link
-              to={`/profile/${user.id}`}
-              className="badge bg-primary rounded-pill"
-            >
-              See More
-            </Link>
-          </ListItem>
-        ))}
-      </ul>
+
+
+    <div className="container">
+      <div class="jumbotron">
+        <h1 class="display-4">Hello,My name is Alex</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">check out to know more</a>
+      </div>
     </div>
+    //   <ul className="list-group list-group">
+    //     {users.map((user) => (
+    //       <ListItem key={user.id}>
+    //         <Profile user={user} />
+    //         {/* Link elements are anchors under-the-hood, but they allow the routing behavior to be controlled by the client rather than the server */}
+    //         <Link
+    //           to={`/profile/${user.id}`}
+    //           className="badge bg-primary rounded-pill"
+    //         >
+    //           See More
+    //         </Link>
+    //       </ListItem>
+    //     ))}
+    //   </ul>
+    // </div>
   );
 }
